6f9754c51cefca79c47c3eb3130f3d755a2ba952
Vitor Mattos <vitor@php.rio>
2022-12-12 21:28:01 -0300

    Add large dumps

---
+++ e-cidade-2018-2-demo.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:faf6bc1c4c492cdd2039096b969be296a40be411b075b183d510e153a0e6177c
+size 409469212
---
+++ e-cidade-2018-2-schema.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca16c8acf7af1cd132ddc0a0f253e874de7dcf63b659f802bb0a1c3e52f9ba30
+size 204220314
